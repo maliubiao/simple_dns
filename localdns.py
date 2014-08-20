68eeceae38f7b609cfb32ab6ae1fe0e4ada00691
maliubiao <maliubiao@gmail.com>
2014-08-20 23:04:29 +0800

    add wait_request_select

--- localdns.py
+++ localdns.py
@@ -21,14 +21,21 @@ REQUEST_TIMEOUT = 10
 #ident -> client
 itable = {} 
 
+has_epoll = False
+
+if hasattr(select, "epoll"):
+    has_epoll = True 
+
+
 def set_globals(): 
     g["sock"] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     g["sockfd"] = sock.fileno()
     sock.bind(SERVER) 
     sock.settimeout(0)
-    g["p"] = select.epoll()
-    p.register(sockfd, select.EPOLLIN |select.EPOLLERR) 
+    if has_epoll:
+        g["p"] = select.epoll()
+        p.register(sockfd, select.EPOLLIN |select.EPOLLERR) 
 
 
 def cat_test(packet):
@@ -59,7 +66,7 @@ def handle_pollin():
     finally:
         b.close() 
     if unknown:
-        ident = struct.unpack(">H", data[:2])[0]
+        ident = struct.unpack(">H", data[:2])[0] 
     else:
         ident = ret["ident"] 
         if cat_test(ret): 
@@ -73,14 +80,15 @@ def handle_pollin():
         sock.sendto(data, client) 
         del itable[ident]
     else: 
-        for k in ret["question"]: 
-            if "name" in k:
-                print "query name: ", k["name"] 
+        if not unknown:
+            for k in ret["question"]: 
+                if "name" in k:
+                    print "query name: ", k["name"] 
         itable[ident]  = (client, time.time())
         sock.sendto(data, DNS_SERVER) 
 
 
-def wait_request(timeout): 
+def wait_request_epoll(timeout): 
     for fd, event in p.poll(timeout): 
         if event & select.EPOLLIN: 
             try:
@@ -90,6 +98,17 @@ def wait_request(timeout):
         if event & select.EPOLLERR:
             raise Exception("Failed") 
 
+def wait_request_select(timeout):
+    r, _, e = select.select([sockfd], [], [sockfd], timeout)    
+    if sockfd in r:
+        try:
+            handle_pollin()
+        except IOError:
+            pass
+    if socket in e:
+        raise Exception("Failed")
+
+
 def clients_gc():
     now = time.time()
     for k, v in itable.items(): 
@@ -98,6 +117,9 @@ def clients_gc():
 
 
 def main():
+    wait_request = wait_request_select
+    if has_epoll:
+        wait_request = wait_request_epoll 
     set_globals() 
     end = 0
     prev = time.time()
